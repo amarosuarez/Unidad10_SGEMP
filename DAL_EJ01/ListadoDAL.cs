40a29624c3579fd3f86d5c31fe1e9ad4223c9b70
amarosuarez <amaro.suarez@iesnervion.es>
2024-11-18 14:35:10 +0100

    Clase persona hecha

---
+++ DAL_EJ01/ListadoDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_EJ01
+{
+    public class ListadoDAL
+    {
+        /// <summary>
+        /// Esta función estática devuelve un listado de personas de la base de datos de azure.
+        /// </summary>
+        /// <returns>Listado de personas</returns>
+        public static List<clsPersona> listadoCompletoPersonasDAL()
+        {
+            List<clsPersona> listadoPersonas;
+
+            // TODO get from DB
+
+            return listadoPersonas;
+        }
+
+    }
+}
---
+++ ENT/clsPersona.cs
@@ -0,0 +1,56 @@
+﻿namespace ENT
+{
+    public class clsPersona
+    {
+        #region Atributos
+        private int id;
+        private String nombre;
+        private String apellidos;
+        private long telefono;
+        private String direccion;
+        private String foto;
+        private DateTime fechaNacimiento;
+        private int idDepartamento;
+        #endregion
+
+        #region Constructores
+        public clsPersona(int id, string nombre, string apellidos, long telefono, string direccion, string foto, DateTime fechaNacimiento, int idDepartamento)
+        {
+            if (id > 0)
+            {
+                this.id = id;
+            }
+
+            if (!string.IsNullOrEmpty(nombre)) {
+                this.nombre = nombre;
+            }
+
+            if (!String.IsNullOrEmpty(apellidos))
+            {
+                this.apellidos = apellidos;
+            }
+
+            if (telefono > 0)
+            {
+                this.telefono = telefono;
+            }
+
+            if (!string.IsNullOrEmpty(direccion))
+            {
+                this.direccion = direccion;
+            }
+
+            if (!string.IsNullOrEmpty(foto))
+            {
+                this.foto = foto;
+            }
+
+            this.fechaNacimiento = fechaNacimiento;
+
+            if (idDepartamento > 0) { 
+                this.idDepartamento = idDepartamento;
+            }
+        }
+        #endregion
+    }
+}
